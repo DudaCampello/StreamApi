60cb550c75f64218d0e7cf3fe9b093bb013b2dab
Eduarda <eduarda.campello@hotmail.com>
2022-10-10 16:23:02 -0300

    exemplo lambda

---
+++ src/br/com/dio/collection/map/exercicioProposto03/Contato.java
@@ -0,0 +1,28 @@
+package br.com.dio.collection.map.exercicioProposto03;
+
+public class Contato {
+    String nome;
+    Integer numero;
+
+    public Contato(String nome, Integer numero) {
+        this.nome = nome;
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+}
+
